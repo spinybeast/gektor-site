d7a89bcbf93f3c92b608e9c28eaa627c1307e860
spiny <spiny_beast@mail.ru>
2015-02-10 17:28:35 +0300

    head

--- config/params.php
+++ config/params.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'siteName' => 'Компания "Гектор"',
+    'siteName' => 'ТД "Гектор"',
+    'sitePhone' => '+ 7 (908) 555 33 22',
     'description' => 'Котлы и не только',
     'keywords' => 'Котлы, котляшки, котло',
     'adminEmail' => 'spiny.beast@gmail.com',
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,48 +28,49 @@ AppAsset::register($this);
 <body>
 
 <?php $this->beginBody() ?>
-    <div class="wrap">
-        <header>
-            <div class="headerdetails">
-        <div class="container">
-            <a class="logo pull-left" href="<?= Url::to('') ?>">
-                <?= Html::img('img/logo.png', array('alt' => Html::encode(Yii::$app->params['siteName']))) ?>
-                <?= Html::encode(Yii::$app->params['siteName']) ?>
-            </a>
-            <div class="pull-right">
-                <div class="call"><?= FA::icon('phone')->size(FA::SIZE_LARGE) ?> <span class="beige">+ 8 (908) 555 33 22</span></div>
-                <ul class="pull-right">
+<div class="wrap">
+    <header>
+        <div class="headerdetails">
+            <div class="container">
+                <a class="logo pull-left" href="<?= Url::to(['site/index']) ?>">
+                    <?= Html::img('img/logo.png', array('alt' => Html::encode(Yii::$app->params['siteName']))) ?>
+                    <span class="beige">&nbsp;<?= Html::encode(Yii::$app->params['siteName']) ?></span>
+                </a>
+
+                <div class="pull-right text-center">
+                    <div class="call">
+                        <?= FA::icon('phone')->size(FA::SIZE_LARGE) ?>
+                        <span class="beige"><?= Html::encode(Yii::$app->params['sitePhone']) ?></span>
+                    </div>
+
                     <?= Html::button(
-                    FA::icon('question') .' Задать вопрос',
-                    [
-                        'class' => 'btn btn-success',
-                        'value' => Url::to(['show-message-form']),
-                        'id' => 'modalButton'
-                    ]) ?>
-
-
-                    <?php
-                    Modal::begin([
-                        'id' => 'modal',
-                        'header' => 'Обратная связь'
-                    ]);
-
-                    echo "<div id='modalContent'></div>";
-
-                    Modal::end();
-                    ?>
-                </ul>
-            </div>
-            <!--Top Search -->
-            <div class="pull-right topsearch">
-                <form class="form-inline">
-                    <input type="search" placeholder="Search Here" class="form-control">
-                    <button data-original-title="Search" class="btn btn-small btn-default"> <?= FA::icon('search')->size(FA::SIZE_LARGE) ?> </button>
-                </form>
+                        FA::icon('question') . '&nbsp;&nbsp;Задать вопрос',
+                        [
+                            'class' => 'btn btn-success',
+                            'value' => Url::to(['show-message-form']),
+                            'id' => 'modalButton'
+                        ]) ?>
+                </div>
+                <?php
+                Modal::begin([
+                    'id' => 'modal',
+                    'header' => 'Обратная связь'
+                ]);
+                echo "<div id='modalContent'></div>";
+                Modal::end();
+                ?>
+                <!--Top Search -->
+                <div class="pull-right topsearch">
+                    <form class="form-inline">
+                        <input type="search" placeholder="Поиск по сайту" class="form-control">
+                        <button data-original-title="Search"
+                                class="btn btn-small btn-default"> <?= FA::icon('search')->size(FA::SIZE_LARGE) ?> </button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-        <?php
+        <div class="navbar-wrapper">
+            <?php
             NavBar::begin([
                 'brandLabel' => '',
                 'brandUrl' => Yii::$app->homeUrl,
@@ -78,7 +79,7 @@ AppAsset::register($this);
                 ],
             ]);
             echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
+                'options' => ['class' => 'navbar-nav'],
                 'items' => [
                     ['label' => 'Главная', 'url' => ['/site/index']],
                     ['label' => 'Каталог оборудования', 'url' => ['/site/catalog']],
@@ -87,21 +88,23 @@ AppAsset::register($this);
                 ],
             ]);
             NavBar::end();
-        ?>
-        </header>
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= $content ?>
+            ?>
         </div>
+    </header>
+    <div class="container">
+        <?=
+        Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= $content ?>
     </div>
+</div>
 
-    <footer class="footer">
-        <div class="container">
-            <p class="text-center">&copy; <?= Yii::$app->params['siteName'] ?> 2011-<?= date('Y') ?></p>
-        </div>
-    </footer>
+<footer class="footer">
+    <div class="container">
+        <p class="text-center">&copy; <?= Yii::$app->params['siteName'] ?> 2011-<?= date('Y') ?></p>
+    </div>
+</footer>
 
 <?php $this->endBody() ?>
 </body>
