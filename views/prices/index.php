56b96820706dc4ea6056a08247da070c94ad68ee
spiny <spiny_beast@mail.ru>
2015-02-26 18:28:41 +0300

    products

--- views/prices/index.php
+++ views/prices/index.php
@@ -10,10 +10,19 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php if (!empty($categories)) { ?>
         <?php foreach ($categories as $category) { ?>
             <?php if (!empty($category->products)) { ?>
-                <h3><?= $category->name ?></h3>
+                <h3 class="beige text-center"><?= $category->name ?></h3>
+                <table class="table table-responsive table-hover table-bordered">
+                <tr>
+                    <th class="text-center">Товар</th>
+                    <th class="text-center">Цена</th>
+                </tr>
                 <?php foreach ($category->products as $product) { ?>
-                    <span><?= $product->name ?></span><?= $product->price ?>
+                    <tr>
+                        <td class="col-md-8"><?= $product->name ?></td>
+                        <td class="col-md-4"><?= $product->price ?></td>
+                    </tr>
                 <?php } ?>
+                </table>
             <?php } ?>
         <?php } ?>
     <?php } ?>
