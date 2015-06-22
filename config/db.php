072e094518d7ffdb277d68e387a1105fefdd3d4a
spiny <spiny_beast@mail.ru>
2015-06-22 19:33:00 +0300

    админка

--- config/db.php
+++ config/db.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
+$local = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=gektor',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 ];
+
+$production = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=tdgektor_db',
+    'username' => 'tdgektor_admin',
+    'password' => 'psIT41K5',
+    'charset' => 'utf8',
+];
+
+return $production;
